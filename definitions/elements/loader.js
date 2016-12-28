b0416045788706ed471df421fe689a821a0dae16
Corey Crowden <corey.crowden@gmail.com>
2016-12-28 21:33:57 +1100

    adjusted loader and added doc page

---
+++ definitions/elements/loader.js
@@ -0,0 +1,19 @@
+/**
+ * Loader
+ *
+ * The loader by default will detect the class .loading and apply the class to $loader until it is removed.
+ */
+
+//
+// $(document).ready(function() {
+//
+//   $('#loader-toggle').click(function () {
+//     $("#loader").toggleClass("loading");
+//   });
+//
+//   $('#loader').click(function () {
+//     $("#loader").toggleClass("loading");
+//   });
+//
+//
+// });
